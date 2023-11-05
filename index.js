4bfae0036e1850857cbbd8a222cd6f709ee33be9
poorneshvishnole <purneshvishnole@gmail.com>
2023-11-06 00:44:09 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 //Configure dotenv
 dotenv.config({ path: "./config/config.env" });
@@ -9,12 +10,18 @@ require("./config/db-connection");
 //Creating an app from express
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 const userRoutes = require("./routes/v1/user.route");
 const courseRoutes = require("./routes/v1/course.route");
 const errorMiddleware = require("./middlewares/error.middleware");
 
 //Using express.json to get request of json data
-app.use(express.json({limit:"1gb"}));
+app.use(express.json({ limit: "1gb" }));
 
 //Configuring cookie-parser
 app.use(cookieParser());
